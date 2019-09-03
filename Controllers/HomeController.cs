397de4cb109e0c622a1cc4e5acc71957d3937bee
Lucas <lwolfs@my.wctc.edu>
2019-09-03 18:32:15 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Northwind
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
