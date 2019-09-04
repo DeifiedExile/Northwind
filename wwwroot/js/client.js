8b6b18902f7056adaaa011da0ea2857255fbf407
Lucas <lwolfs@my.wctc.edu>
2019-09-03 20:38:55 -0500

    Toasts and buttons and forms

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,18 @@
 ﻿$(function(){
     //alert("Hello World");
 });
+$(document).ready(function () {
+    
+    var toastAudio = new Audio('/Media/toast.wav');
+
+    $('#clickMe').on('click', function () {
+        $('.toast').toast({ autohide: false }).toast('show');
+        toastAudio.currentTime = 0;
+        toastAudio.play();
+    });
+    $('.btnCloseToast').on('click', function () {
+        $(this).parents().toast('hide');
+    })
+
+});
+
