25601b368292f4c46886553054f70631750211c8
Lucas <lwolfs@my.wctc.edu>
2019-09-03 20:58:29 -0500

    bloons

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -3,16 +3,19 @@
 });
 $(document).ready(function () {
     
-    var toastAudio = new Audio('/Media/toast.wav');
+    //var toastAudio = new Audio("~/Media/toast.wav");
 
     $('#clickMe').on('click', function () {
         $('.toast').toast({ autohide: false }).toast('show');
-        toastAudio.currentTime = 0;
-        toastAudio.play();
+       // toastAudio.currentTime = 0;
+       // toastAudio.play();
     });
     $('.btnCloseToast').on('click', function () {
         $(this).parents().toast('hide');
     })
 
+    $('#birthday').datepicker();
+
+    $('#bloon').draggable();
 });
 
