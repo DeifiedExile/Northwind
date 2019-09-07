68ac00a62a2f1471b5222d0fbebe7f79e8be07ed
Lucas <lwolfs@my.wctc.edu>
2019-09-07 16:12:38 -0500

    Added content to cards. Configured toast to appear with discount info in upper right. Configured toast to be dismissed on esc keydown

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -2,17 +2,35 @@
     //alert("Hello World");
 });
 $(document).ready(function () {
+    console.log('here');
     
-    var toastAudio = new Audio('');
-    toastAudio.src = "~/media/toast.wave";
+    //toastAudio.src = "~/media/toast.wave";
 
-    $('#clickMe').on('click', function () {
+    $('.clickMe').on('click', function () {
+        console.log($(this).data('sale'));
         $('.toast').toast({ autohide: false }).toast('show');
-        toastAudio.currentTime = 0;
-        toastAudio.play();
+        if ($(this).data('sale') === 1) {
+            $('#discountCode').html('daily2019');
+            $('#tstTitle').html('Your Daily Discount Code');
+        }
+        else if ($(this).data('sale') === 10) {
+            $('#discountCode').html('dec2019');
+            $('#tstTitle').html('Your Decade Discount Code');
+        }
+        else if ($(this).data('sale') === "6e6f74206120726f626f74") {
+            $('#discountCode').html('gorn2019');
+            $('#tstTitle').html('Your Consumables Discount Code');
+        }
+        //toastAudio.currentTime = 0;
+        //toastAudio.play();
     });
     $('.btnCloseToast').on('click', function () {
         $(this).parents().toast('hide');
+    })
+    $(document).on('keyup', function (e) {
+        if (e.which === 27) {
+            $('.toast').toast('hide');
+        }
     })
 
     $('#birthday').datepicker();
