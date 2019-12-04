3f7edc80289d514ebd3f897d6e896373cff9dd1b
Lucas <lwolfs@my.wctc.edu>
2019-12-03 18:12:34 -0600

    Pre-chartjs commit

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -26,50 +26,7 @@ $(document).ready(function () {
         }
     });
 
-    //$('#birthDate').datepicker();
-
-    //$('#bloon').draggable();
-
-    //$('.form-check-input').each(function () {
-    //    $(this).prop('checked', false);
-    //});
-
-    //$('.blnCheck').on('change', function () {
-        
-    //    $('#img' + this.id).css('visibility', 'visible');
-
-    //    $(this).is(':checked') ? $('#img' + this.id).removeClass().addClass('animated slideInUp') : $('#img' + this.id).addClass('animated slideOutUp');
-    //});
-
-    //var animEffect;
-    //var selector = Math.floor((Math.random() * 5) + 1);
-    //switch (selector) {
-    //    case 1: animEffect = 'heartBeat';
-    //        break;
-    //    case 2: animEffect = 'wobble';
-    //        break;
-    //    case 3: animEffect = 'flip';
-    //        break;
-    //    case 4: animEffect = 'lightSpeedIn';
-    //        break;
-    //    case 5: animEffect = 'jackInTheBox';
-    //        break;
-    //}
-
-    //$('#msgBDay').addClass(animEffect);
-
-    //$('#checkAll').on('change', function () {
-    //    $('.blnCheck').each(function () {
-    //        $(this).click();
-    //    });
-    //});
-
-    //$('#bDaySubmit').on('click', function () {
-        
-    //    if (!$('.blnCheck:checked').length > 0) {
-    //        $('#toastBDay').toast({ autohide: false }).toast('show');
-    //    }
-    //});
+    
 
 });
 
